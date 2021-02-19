4dbe63def63d4331025defe96cb3b8bf758e3de1
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-02-19 15:57:19 +0100

    added the solution to the exercise-1.js

--- week-1/extraExercises/README.md
+++ week-1/extraExercises/README.md
@@ -1,4 +1,6 @@
 Exercises
+In exercise-1.js
+
 1. Create the following variables
  - name, which is a string set to your current name
  - dayOfBirth, which is a number set to the day of your birth month
@@ -6,5 +8,3 @@ Exercises
 3. Write some JavaScript that prompt's the user for their favorite color. Once the user has submitted a favorite color, log that color to the console along with a friendly message.
 4. Create a string that contains both single quotes and double quotes.
 5. What is the difference between null and undefined?
-7. What is NaN in JavaScript? What is the typeof NaN?
-8. You can declare a variable by typing let thing;. What is the value of thing?
---
+++ week-1/extraExercises/exercise-1.js
@@ -0,0 +1,28 @@
+//Exercise 1
+let firstName = "Enia";
+let dayOfBirth = "10.08.1995";
+let dayBirth = 1995;
+console.log(firstName);
+console.log(dayOfBirth);
+console.log(typeof firstName); //string
+console.log(typeof dayOfBirth); //string
+console.log(typeof dayBirth); //number
+
+//Exercise 4
+let phrase1 = 'He said, "I haven\'t been to Andorra yet", the other day.';
+console.log(phrase1);
+
+let phrase2= 'Hello, "I just wanted to let you know that I won\'t make it to the dinner tonight"';
+console.log(phrase2);
+
+//Trying out the dollar sign also
+var a = 5;
+var b = 10;
+console.log(`Sum is equal to ${a+b}`); 
+
+//Exercise 5
+let noValue = undefined; //undefined = value not assigned
+let emailAddress = null; //null = interntional absence of data
+console.log(noValue);
+console.log(emailAddress);
+
