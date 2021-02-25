0856c8f12ae791f07b2cd436107312e56f191253
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-02-25 12:54:50 +0100

    added exercise-K.js in week-1/InClass

--- week-2/InClass/exercise-J.js
+++ week-2/InClass/exercise-J.js
@@ -1,6 +1,9 @@
 function secondMatchesAmy(array) {
-  if ( ) {
+  if (array[1] === "Amy") {
     return "Second index matched!";
   }
   return "Second index not matched";
 }
+
+console.log(secondMatchesAmy(["Maria","Amy", "Luca", "Mariana"]));
+console.log(secondMatchesAmy(["Miriam", "Enola"]));
---
+++ week-2/InClass/exercise-K.js
@@ -0,0 +1,18 @@
+const studentsMigracode = [
+    "Kiki",
+    "Keti",
+    "Arthur",
+    "Kris",
+    "Manuel",
+    "Adrian",
+    "Simon",
+    "Guido",
+    "Kimberly",
+    "Eli",
+    "Manpreet",
+    "Enia"
+]
+
+for (let i=0; i<studentsMigracode.length; i++){
+    console.log(i);
+}
