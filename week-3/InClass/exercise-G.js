5fdfeefe00bd3051ef68d9fe80e80cc5a1fc2ef7
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-03-04 14:45:07 +0100

    updated solution for InClass/exercise-G.js

--- week-3/InClass/exercise-G.js
+++ week-3/InClass/exercise-G.js
@@ -10,3 +10,21 @@ function isHighScore(score){
 
 const isHighScores = scores.filter(isHighScore);
 console.log(isHighScores);
+
+let birthYears = [ 1964, 2008, 1999, 2005, 1978, 1985, 1919 ];
+
+
+function calculateTheAge(birthYear){
+    return 2021 - birthYear;
+}
+
+function legalAgeForDriving(birthYear){
+    const age = calculateTheAge(birthYear);
+    if(age >= 17){
+        return `Born in ${birthYear} can drive`;
+    } else {
+        return `Born in ${birthYear}, can drive in ${17-age}`;
+    }
+}
+const fileteredBirthYears = birthYears.filter(legalAgeForDriving);
+console.log(`These are the birth years of people who can drive: ${fileteredBirthYears.join(", ")}`);
